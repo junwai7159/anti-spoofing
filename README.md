749b40e9916f0abc7d4bd5147f7b44bd2695eeb9
junwai7159 <junwai7159@gmail.com>
2024-08-29 15:23:56 +0700

    Added ONNX

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
-# Usage
-## Docker
-### Build the Docker imagte
+# Anti-spoofing with YOLOv8
+
+## Usage
+### Docker
+#### Build the Docker imagte
 ```
 docker build -t yolo_streamlit_app .
 ```
 
-### Run the Docker container
+#### Run the Docker container
 ```
 docker run -p 8501:8501 yolo_streamlit_app
 ```
 
-## Streamlit
-### Test mode
+### Streamlit
+#### Test mode
 ```
 streamlit run app.py
 ```
+
+## Demonstration
+Link: https://junwai7159-anti-spoofing-app-3onp8m.streamlit.app/
--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import streamlit as st
 from ultralytics import YOLO
 import cv2
+import onnxruntime as ort
+import time
 import numpy as np
+from utils import draw
 
-# Load the YOLOv8 model
+# Load the YOLOv8 model (ONNX)
+# session = ort.InferenceSession('./runs/detect/train/weights/best.onnx')
 model = YOLO('./runs/detect/train/weights/best.pt')
 
 st.title("YOLOv8 Object Detection")
@@ -16,9 +20,12 @@ if uploaded_file is not None:
     img = cv2.imdecode(file_bytes, 1)
     
     # Run inference
-    print('Inference')
+    st.write('Inferencing ...')
+    start_time = time.time()
     results = model.predict(source=img)
-    print('Done')
+    end_time = time.time()
+    inference_time = end_time - start_time
+    st.write(f'Inference time: {inference_time:.2f}')
     
     # Draw bounding boxes
     for result in results:
@@ -27,7 +34,7 @@ if uploaded_file is not None:
             label = 'spoof' if int(box.cls.item()) else 'live'
             confidence = box.conf.item()
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.putText(img, f'{label} {confidence:.2f}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            cv2.putText(img, f'{label} {confidence:.2f}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 2)
     
     # Display the results
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
